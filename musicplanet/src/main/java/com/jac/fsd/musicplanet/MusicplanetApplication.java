0bdd0ac575c8b75c392a0c8b139bc8f4ec03f18f
老房东 <huangdong@gmail.com>
2024-06-23 11:56:59 -0400

    add dotenv support and load .env in Application

--- musicplanet/src/main/java/com/jac/fsd/musicplanet/MusicplanetApplication.java
+++ musicplanet/src/main/java/com/jac/fsd/musicplanet/MusicplanetApplication.java
@@ -1,5 +1,6 @@
 package com.jac.fsd.musicplanet;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusicplanetApplication {
 
     public static void main(String[] args) {
+        /*
+         * Load the .env file and set the environment variables
+         */
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(MusicplanetApplication.class, args);
     }
 
