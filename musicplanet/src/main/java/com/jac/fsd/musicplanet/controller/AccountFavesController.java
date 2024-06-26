2f390e39a2eacba990bcbbeda66e3289ca4c98e2
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2024-06-26 00:41:44 -0400

    some more view setup, displaying a list of Album objects on a page

--- musicplanet/src/main/java/com/jac/fsd/musicplanet/controller/AccountFavesController.java
+++ musicplanet/src/main/java/com/jac/fsd/musicplanet/controller/AccountFavesController.java
@@ -1,15 +1,27 @@
 package com.jac.fsd.musicplanet.controller;
 
+import com.jac.fsd.musicplanet.model.Album;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class AccountFavesController {
 
     @GetMapping("/user/favorites")
     public String favorites(Model model) {
-        model.addAttribute("customvar", "baboom");
+        List<Album> hardCoded = new ArrayList<>(Arrays.asList(
+                new Album("Dark Side of the Moon", 1973),
+                new Album("Meteora", 2003),
+                new Album("The Memory of Trees", 1995)
+        ));
+        model.addAttribute("albumList", hardCoded);
+        model.addAttribute("customvar", "testMessage");
         return "favorites";
     }
+
 }
