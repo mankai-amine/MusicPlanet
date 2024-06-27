f92e325b38fa2341a98254da0ab7d34695a6b15d
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2024-06-27 17:07:51 -0400

    removing old changes to homecontroller

--- musicplanet/src/main/java/com/jac/fsd/musicplanet/controller/HomeController.java
+++ musicplanet/src/main/java/com/jac/fsd/musicplanet/controller/HomeController.java
@@ -10,14 +10,4 @@ public class HomeController {
     public String index(){
         return "login";
     }
-
-    @GetMapping("/home")
-    public String home(){
-        return "home";
-    }
-
-    @GetMapping("/register")
-    public String register(){
-        return "register";
-    }
 }
