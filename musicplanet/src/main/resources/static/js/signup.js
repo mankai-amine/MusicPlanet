4a216bf087eaa02b8e0a3f74aaf47b6b71dd5d3a
老房东 <huangdong@gmail.com>
2024-06-28 00:16:48 -0400

    change me.html to search.html

--- musicplanet/src/main/resources/static/js/signup.js
+++ musicplanet/src/main/resources/static/js/signup.js
@@ -18,7 +18,7 @@ const signup = (event) => {
           new Date(currentDate).getTime() + 30 * 24 * 60 * 60 * 1000;
         const expires = expiresData.toLocaleString();
         document.cookie = `token=${token}; expires=${expires}; path=/`;
-        window.location.replace("/me.html");
+        window.location.replace("/search.html");
       });
     }
   });
